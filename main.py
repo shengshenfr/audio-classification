ea93667694dcecc0b80471ce9e22004396238722
shen sheng <sheng.shen@telecom-bretagne.eu>
2018-03-26 15:54:46 +0200

    add convert mp3 to wav

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import mp3_to_wav
+import sh
+
+def main():
+    mp3_file = "mp3/music.mp3"
+    wav_dir = "wav"
+    mfcc_dir = "feature"
+
+
+
+    # modi_file = "feature/propriete.txt"
+	# result_file = "result/result.txt"
+
+    mp3_to_wav.run(mp3_file, wav_dir, mfcc_dir)
+
+
+
+
+
+if __name__ == '__main__':
+	main()    
